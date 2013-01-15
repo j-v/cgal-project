496dee3612079a22b95ce13737c4a90005631778
Jon Volkmar <jonathonvolkmar@gmail.com>
2013-01-15 14:41:27 +0100

    Implemented intensity gradient gradient magnitude computation

--- test/CannyHarris/CannyHarris.cpp
+++ test/CannyHarris/CannyHarris.cpp
@@ -48,6 +48,34 @@ int main( int argc, char** argv )
   namedWindow("Harris Corner Detection", CV_WINDOW_AUTOSIZE );
   imshow( "Harris Corner Detection", copy );
 
+  // refered to http://stackoverflow.com/questions/5787938/how-to-find-the-intensity-gradient-in-canny-edge-detector 
+  // Use Sobel operator to get X and Y partial intensity derivatives, then create matrix of gradient intensity
+  Mat grad; // gradient intensity
+  int ddepth = CV_16S; // ?
+  int scale = 1;
+  int delta = 0;
+
+  /// Generate grad_x and grad_y
+  Mat grad_x, grad_y;
+  Mat abs_grad_x, abs_grad_y;
+
+  /// Gradient X
+  //Scharr( src_gray, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
+  Sobel( gray_img, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
+  convertScaleAbs( grad_x, abs_grad_x );
+
+  /// Gradient Y
+  //Scharr( src_gray, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
+  Sobel( gray_img, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
+  convertScaleAbs( grad_y, abs_grad_y );
+
+  /// Total Gradient (approximate)
+  addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
+
+  // Display gradient intensity image
+  namedWindow("Intensity Gradient Magnitude", CV_WINDOW_AUTOSIZE );
+  imshow( "Intensity Gradient Magnitude", grad );
+
   waitKey(0);
 
   return 0;
