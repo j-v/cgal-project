2c4dffec8c404e0382563d52ecd212c5f22c5a0a
Jon Volkmar <jonathonvolkmar@gmail.com>
2013-01-16 18:45:28 +0100

    modified EmdDB interface

--- src/db.h
+++ src/db.h
@@ -4,15 +4,21 @@
 class EmdDB
 {
 public:
+	// dummy constructor, must call load or create after using
+	EmdDB() : numEntries(0) { }; 
+
 	void addEntry(const signature_t & entry);
+
 	signature_t getEntry(unsigned int i);
+
+	// Load DB from path. Return 0 on success, other value otherwise
+	int load(std::string path);
+
+	// Create new DB in path. Return 0 on success, other value otherwise
+	int create(std::string path);
+	
+	// Close the DB, flushing all written data to disk and performing clean-up
 	void close();
 
 	unsigned int numEntries;
-
-	static EmdDB load(std::string path);
-	static EmdDB create(std::string path);
-private:
-	EmdDB();
-
 };
